e1c52bd154a517df7a956bc32b7bd199fc2a743d
Lezonje <jeffreylezonxp@gmail.com>
2021-05-20 10:47:41 +0800

    test

---
+++ src/test/java/org/tron/command/CliTest.java
@@ -0,0 +1,14 @@
+package org.tron.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CliTest {
+    private static final Logger logger = LoggerFactory.getLogger("test");
+
+//    @Test
+//    public void testCli() {
+//        Cli cli = new Cli();
+//        cli.run(Peer.getInstance(Peer.PEER_NORMAL));
+//    }
+}
